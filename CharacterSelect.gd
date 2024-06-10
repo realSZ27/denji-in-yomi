d427b5592d42a82fd40ff15adda4c49db7237146
realSZ27 <99378252+realSZ27@users.noreply.github.com>
2024-06-10 01:25:38 -0500

    Add files via upload

---
+++ CharacterSelect.gd
@@ -0,0 +1,4 @@
+extends "res://ui/CSS/CharacterSelect.gd"
+
+func _ready():
+	addCustomChar("Denji", "res://_Denji/characters/denji/Denji.tscn")
---
+++ ModMain.gd
@@ -0,0 +1,5 @@
+extends Node
+
+func _init(modLoader = ModLoader):
+	modLoader.installScriptExtension("res://_Denji/CharacterSelect.gd")
+
